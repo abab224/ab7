aa9c5aa90d645b197002f9a38621e152bd3f1bbf
abab224 <0003oror@gmail.com>
2024-11-27 15:26:37 +0900

    更新内容を説明するコメント

--- server.js
+++ server.js
@@ -1,45 +1,27 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
-const PORT = process.env.PORT || 10000;
+app.use(express.static(path.join(__dirname, 'public')));
 
-// 静的ファイルの提供
-app.use(express.static('public'));
-
-// ユーザー管理用オブジェクト
-const users = {};
-
-// WebSocketイベント
 io.on('connection', (socket) => {
-  console.log('ユーザーが接続しました');
+  console.log('A user connected');
 
-  socket.on('login', ({ username, password }) => {
-    if (!users[password]) {
-      users[password] = [];
-    }
-    users[password].push({ id: socket.id, username });
-    socket.join(password);
-    socket.emit('loginSuccess', { username });
-  });
-
-  socket.on('message', ({ password, message, username }) => {
-    io.to(password).emit('message', { username, message });
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data); // 全クライアントにメッセージを送信
   });
 
   socket.on('disconnect', () => {
-    console.log('ユーザーが切断しました');
-    Object.keys(users).forEach((password) => {
-      users[password] = users[password].filter((user) => user.id !== socket.id);
-    });
+    console.log('A user disconnected');
   });
 });
 
-// サーバー起動
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
