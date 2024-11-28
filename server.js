75b9202e2a7607495d3eb50bf4dc3f58028ff0b0
abab224 <0003oror@gmail.com>
2024-11-28 15:18:20 +0900

    Update

--- server.js
+++ server.js
@@ -16,26 +16,13 @@ io.on("connection", (socket) => {
   console.log("ユーザーが接続しました");
 
   socket.on("login", (data) => {
-    const { username, password } = data;
-
-    if (!username || !password || password.length !== 4 || isNaN(password)) {
-      socket.emit("loginError", "正しいユーザー名と4桁の数字パスワードを入力してください");
-      return;
-    }
-
-    // ユーザー情報を登録
-    users[socket.id] = username;
-
-    // ログイン成功を通知
-    socket.emit("loginSuccess");
-
-    // 入室メッセージを送信
-    io.emit("message", { username, message: "が入室しました", self: false });
+    users[socket.id] = data.username;
+    io.emit("message", { username: data.username, message: "が入室しました", self: false });
   });
 
   socket.on("message", (message) => {
     const username = users[socket.id] || "匿名";
-    io.emit("message", { username, message, self: socket.id === message.senderId });
+    io.emit("message", { username, message, self: false });
   });
 
   socket.on("disconnect", () => {
